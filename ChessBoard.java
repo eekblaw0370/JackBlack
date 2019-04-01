7a754a46f87b41ab86a4f83dc9cfb9ca26fa2918
Joe Adcock <jradcock2002@gmail.com>
2019-04-01 11:37:21 -0500

    nice

--- ChessBoard.java
+++ ChessBoard.java
@@ -1,33 +1,8 @@
+//Joe Adcock
+//Period 3
+//Handling the board for a chess game
 
-/**
- * Write a description of class ChessBoard here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class ChessBoard
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class ChessBoard
-     */
-    public ChessBoard()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
-}
+    //hi
+} 
