49452f2ded84d21347bf6d096e788d95b8fa9d8b
Evan Ekblaw <eekblaw0370@myduneland.org>
2019-04-01 11:32:41 -0500

    Yes

---
+++ crazyEights.java
@@ -0,0 +1,34 @@
+
+/**
+ * CrazyEights a game like Uno can be played by one player verus the computer to see who can run
+ * out of cards faster than the other.
+ *
+ * Evan Ekblaw
+ * 1.1 - 4/1/2018
+ */
+public class crazyEights
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class crazyEights
+     */
+    public crazyEights()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
