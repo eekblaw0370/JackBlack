45e82f01472077902e8cb61c60b733b7027fcf05
Joe Adcock <jradcock2002@gmail.com>
2019-04-03 12:24:36 -0500

    c

--- ChessBoard.java
+++ ChessBoard.java
@@ -4,36 +4,56 @@
 
 public class ChessBoard
 {
-    Card[][] board = new Card[8][8];
+    private Card[][] board = new Card[8][8];
     public ChessBoard()
     {
-        board[0][0] = new Card(Suit.club, 5);
-        board[0][1] = new Card(Suit.club, 3);
-        board[0][2] = new Card(Suit.club, 1);
-        board[0][3] = new Card(Suit.club, 12);
-        board[0][4] = new Card(Suit.club, 13);
-        board[0][5] = new Card(Suit.club, 2);
-        board[0][6] = new Card(Suit.club, 4);
-        board[0][7] = new Card(Suit.club, 6);
+        this.board[0][0] = new Card(Suit.club, 5);
+        this.board[0][1] = new Card(Suit.club, 3);
+        this.board[0][2] = new Card(Suit.club, 1);
+        this.board[0][3] = new Card(Suit.club, 12);
+        this.board[0][4] = new Card(Suit.club, 13);
+        this.board[0][5] = new Card(Suit.club, 2);
+        this.board[0][6] = new Card(Suit.club, 4);
+        this.board[0][7] = new Card(Suit.club, 6);
         for (int i = 1; i <= 8; i++){
-            board[1][i] = new Card(Suit.spade, i);
+            this.board[1][i] = new Card(Suit.spade, i);
         }
         for (int j = 2; j <= 5; j++){
             for (int i = 1; i <= 8; i++){
-                board[j][i] = new Card(Suit.empty, 0);
+                this.board[j][i] = new Card(Suit.empty, 0);
             }
         }
         for (int i = 1; i <= 8; i++){
-            board[6][i] = new Card(Suit.diamond, i);
+            this.board[6][i] = new Card(Suit.diamond, i);
         }
-        board[7][0] = new Card(Suit.heart, 5);
-        board[7][1] = new Card(Suit.heart, 3);
-        board[7][2] = new Card(Suit.heart, 1);
-        board[7][3] = new Card(Suit.heart, 12);
-        board[7][4] = new Card(Suit.heart, 13);
-        board[7][5] = new Card(Suit.heart, 2);
-        board[7][6] = new Card(Suit.heart, 4);
-        board[7][7] = new Card(Suit.heart, 6);
+        this.board[7][0] = new Card(Suit.heart, 5);
+        this.board[7][1] = new Card(Suit.heart, 3);
+        this.board[7][2] = new Card(Suit.heart, 1);
+        this.board[7][3] = new Card(Suit.heart, 12);
+        this.board[7][4] = new Card(Suit.heart, 13);
+        this.board[7][5] = new Card(Suit.heart, 2);
+        this.board[7][6] = new Card(Suit.heart, 4);
+        this.board[7][7] = new Card(Suit.heart, 6);
+    }
+    
+    public void printBoard(ChessBoard chessboard)
+    {
+        for (int a = 0; a <= 7; a++){
+            System.out.println("┌───┐┌───┐┌───┐┌───┐┌───┐┌───┐┌───┐┌───┐");
+            for (int b = 0; b <= 7; b++){
+                System.out.print("│" + BuildCard.displaySuit(board[a][b]) + "   " + BuildCard.displaySuit(board[a][b]) + "|");
+            }
+            System.out.println();
+            for (int c = 0; c <= 7; c++){
+                System.out.print("│  " + BuildCard.displayRank(board[a][c]) + "  │");
+            }
+            System.out.println();
+            for (int d = 0; d <= 7; d++){
+                System.out.print("│" + BuildCard.displaySuit(board[a][d]) + "   " + BuildCard.displaySuit(board[a][d]) + "|");
+            }
+            System.out.println();
+            System.out.println("└───┘└───┘└───┘└───┘└───┘└───┘└───┘└───┘");
+        }
     }
 } 
 
