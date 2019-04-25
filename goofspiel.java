8f2ed204ab97a540110d961247cf0ed72bf142cc
Aidan Li <flashandfoul@gmail.com>
2019-04-24 22:35:00 -0500

    gorf

--- goofspiel.java
+++ goofspiel.java
@@ -6,18 +6,24 @@
 import java.util.*;
 public class goofspiel{
     public static void main(String[] args){
-        Scanner choicer=new Scanner(System.in);
+        Scanner choicer=new Scanner(System.in);    //the choice is yours
         ArrayList<String> suits=new ArrayList<String>();
-        suits.add("diamonds");suits.add("hearts"); 
-        suits.add("clubs"); suits.add("spades");
-        boolean yessir=false;
-        int choice=0, cbid=0, hbid=0;
+        suits.add("diamonds");suits.add("hearts"); //what fancy suits you have!
+        suits.add("clubs"); suits.add("spades");   //The better to scam you with, my dear!
+        ArrayList<Integer> thedeck=new ArrayList<Integer>();
+        ArrayList<Integer> hhand=new ArrayList<Integer>();
+        ArrayList<Integer> chand=new ArrayList<Integer>();
+        for(int i=1;i<=13;i++){thedeck.add(i); hhand.add(i); chand.add(i);}   //this be the stuff
+        Collections.shuffle(thedeck);             //Guess what this does
+        boolean yessir=false;                     //I actually forgot what this does
+        int choice=0, cbid=0, hbid=0;             //The bids
+        int temp=0;                               //It's okay, mom, it's temporary
         Random randy=new Random();                //wow! look at all these handy variables!
-        String hsuit="", csuit="";
+        String hsuit="", csuit="", dsuit="";
+        int sparklebag=0;                         //deploy the sparkebag
         int h=0,c=1, pval=0;
-        Card prize;
+        int prize;
         int hscore=0,cscore=0;
-        int[][] hands=new int[2][13];
         while(hsuit!="diamonds" && hsuit!="hearts" && hsuit!="clubs" && hsuit!="spades"){
             System.out.println("Choose your suit. (d/h/c/s)");
             hsuit=choicer.nextLine();
@@ -29,33 +35,42 @@ public class goofspiel{
                 default: System.out.println("Nein! Dost thou not know the meaning of words?"); break;
             }
         } 
-        csuit=suits.get(randy.nextInt(2));
+        csuit=suits.remove(randy.nextInt(2));
+        dsuit=suits.get(randy.nextInt(1));
         printvs(hsuit,csuit);
-        for(int n=0;n<2;n++){
-            for(int i=0;i<13;i++){
-                hands[n][i]=i+1;
-            }
-        }
-        Deck pile=new Deck();
-        pile.shuffle();
         for(int i=0;i<13;i++){
-            prize=pile.deal();
-            pval=prize.getRank();
-            System.out.println("Current prize: "+prize);
+            prize=thedeck.remove(randy.nextInt(thedeck.size()));  //2 levels of random
+            System.out.println("Current prize: "+prize+" of "+dsuit);
             System.out.println("Here are your bid options: ");
-            for(int n:hands[h]){
-                if(n!=0){System.out.print(n+" ");}
-                else{System.out.print("  ");}
-            }
+            for(int n:hhand){System.out.println(n+" ");}
             System.out.println("Choose a number to bid.");
-            while(!yessir){
-            choice=choicer.nextInt();
-            hbid=hands[h][choice-1];
-            if(hbid!=0){yessir=true;}
-            else{System.out.println("No! Not available!");}
-        }
-        //dostuff
-        yessir=false;
+            while(true){
+            hbid=choicer.nextInt();
+            if(hhand.contains(hbid))break;
+            else print("You fool! Choose again!");
+           }
+           temp=randy.nextInt(2)+prize-1;
+           if(chand.contains(temp)){cbid=chand.remove(chand.indexOf(temp));}
+           else if(temp<prize){
+               while(true){
+                   
+                }
+           }
+           else{
+               
+            }
+           System.out.println("Computer's bid: "+cbid+" of "+csuit);
+           if(hbid>cbid){
+               hscore+=prize+sparklebag; sparklebag=0;
+            }
+           else if(cbid>hbid){
+               cscore+=prize+sparklebag; sparklebag=0;
+            }
+           else{
+               sparklebag+=prize;
+               System.out.println("Tie bid. Bonus prize is now at "+sparklebag);
+            }
+           
         }
         System.out.println();
         if(hscore==cscore){System.out.println("Tie!");}
@@ -63,19 +78,19 @@ public class goofspiel{
         else{System.out.println("Computer wins!");}
     }
     public static void printvs(String human, String computer){
-        print("       _____             /                                               ");
-        print("     _/ _ _ \\_          /       ___________                              ");
-        print("    (o / | \\ o)        /       |.---------.|                             ");
-        print("     || o|o ||        /        ||         ||                              ");
-        print("     | \\_|_/ |       /         ||         ||                             ");
-        print("     |  ___  |      /_V_S__    ||         ||                              ");
-        print("     | (___) |            /    |'---------'|                              ");
-        print("     |\\_____/|           /      `)__ ____('                              ");
-        print("     | \\___/ |          /       [=== -- o ]--.                           ");
-        print("     \\       /         /      __'---------'__ \\                         ");
-        print("      \\__ __/         /      [::::::::::: :::] )                         ");
-        print("                     /        `\"\"'\"\"\"\"\"'\"\"\"\"`/T\\             ");  
-        print("                                             \\_/                        ");
+        print("       _____             /                                         ");
+        print("     _/ _ _ \\_          /       ___________                       ");
+        print("    (o / | \\ o)        /       |.---------.|                      ");
+        print("     || o|o ||        /        ||         ||                       ");
+        print("     | \\_|_/ |       /         ||         ||                      ");
+        print("     |  ___  |      /_V_S__    ||         ||                       ");
+        print("     | (___) |            /    |'---------'|                       ");
+        print("     |\\_____/|           /      `)__ ____('                       ");
+        print("     | \\___/ |          /       [=== -- o ]--.                    ");
+        print("     \\       /         /      __'---------'__ \\                  ");
+        print("      \\__ __/         /      [::::::::::: :::] )                  ");
+        print("                     /        `\"\"'\"\"\"\"\"'\"\"\"\"`/T\\       ");  
+        print("                                             \\_/                  ");
         print("   YOU(The Goof)           COMPUTER(The Spiel)                     ");
         System.out.println("      "+human+"                    "+computer);
     }
